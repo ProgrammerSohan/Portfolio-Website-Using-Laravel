f9e2fc20cfdffdb4a8e65de7782b983eead1aed9
ProgrammerSohan <sohanofficial2022@gmail.com>
2026-03-26 23:30:41 +0600

    17.1-hero section part9

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,14 @@
 import './bootstrap';
 
-import 'laravel-datatables-vite';
+//import 'laravel-datatables-vite';
+
+//import 'https://cdn.datatables.net/staterestore/1.4.3/js/dataTables.stateRestore.min.js';
+
+//import 'https://cdn.datatables.net/staterestore/1.4.3/js/stateRestore.bootstrap4.min.js';
+import 'https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js';
+import 'https://cdn.datatables.net/1.13.1/js/dataTables.bootstrap4.min.js';
+
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
